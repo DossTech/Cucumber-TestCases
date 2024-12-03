2c0d1a5d76acb320e90665f99d99646c889ff730
MANGO <MANGO@BLACKHOLE.hgu_lan>
2024-12-04 00:14:40 +0530

    I commit

---
+++ src/test/java/steps/Hooks.java
@@ -0,0 +1,22 @@
+package steps;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks extends BaseClass{
+	
+	@Before
+	public void precondition() {
+		driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+	}
+	
+	@After
+	private void postcondition() {
+		driver.close();
+
+	}
+
+}
